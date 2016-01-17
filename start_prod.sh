41116bc80054f98d12ffa6cf8d6124f54b823633
lev <lev@ardefact.com>
2016-01-16 18:45:34 -0800

    Fixing options bug in production startup script
    Reviewed at https://ardefact.com/rb/r/26/

--- start_prod.sh
+++ start_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start production instance with minifacation turned on
-pushd ./ardefact_api/ardefact-api; node server.js -m -- $@
+pushd ./ardefact_api/ardefact-api; node server.js -m $@
